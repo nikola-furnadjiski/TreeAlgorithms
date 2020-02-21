ef6dfed7110f98835730272591964f5a4574d136
Nikola Furnadjiski <nikola.furnadjiski@outlook.com>
2020-02-22 00:01:41 +0100

    -Threaded Binary Tree methods finished

--- src/me/nikola/threadedBinaryTree/TBTree.java
+++ src/me/nikola/threadedBinaryTree/TBTree.java
@@ -47,44 +47,117 @@ public class TBTree<E> {
     }
 
     public TBNode<E> predecessorInorder(TBNode<E> node) {
-        if(node.leftTag == '-')
+        if (node.leftTag == '-')
             return node.left;
 
         TBNode<E> p = node.left;
-        while(p.rightTag == '+')
+        while (p.rightTag == '+')
             p = p.right;
         return p;
     }
 
     public TBNode<E> successorInorder(TBNode<E> node) {
-        if(node.rightTag == '-')
+        if (node.rightTag == '-')
             return node.right;
 
         TBNode<E> p = node.right;
-        while(p.leftTag == '+')
+        while (p.leftTag == '+')
             p = p.left;
         return p;
     }
 
     public void inorderNonRecursive() {
-        if(head.left == head) //is tree is empty
+        if (head.left == head) //is tree is empty
             return;
 
         System.out.print("INORDER (nonrecursive): ");
 
         TBNode<E> node = head.left;
-        while(node.leftTag == '+')
+        while (node.leftTag == '+')
             node = node.left;
 
-        while(node != head) {
+        while (node != head) {
             System.out.print(node.info.toString() + " ");
             node = successorInorder(node);
         }
     }
 
-    public void successorPreorder(TBNode<E> node) {
+    public TBNode<E> successorPreorder(TBNode<E> node) {
+        if (node.leftTag == '+')
+            return node.left;
+        if (node.rightTag == '+')
+            return node.right;
+
+        //if there is no left nor right subtree, successor is the root of right subtree
+        // in which left tree is P (we iterate over nodes while we find a link)
+        TBNode<E> p = node;
+        while(p.rightTag == '-')
+            p = p.right;
+
+        if(p == head)
+            return head; //we reached the head, we can return it or null
+        else
+            return p.right;
 
     }
 
+    public void preorderNonRecursive() {
+        if(head.leftTag == '-')
+            return; //empty tree
 
+        System.out.println("PREORDER (nonrecursive): ");
+
+        TBNode<E> p = head.left; //i.e. root
+
+        while(p != head) {
+            System.out.print(p.info.toString() + " ");
+            p = successorPreorder(p);
+        }
+        System.out.println();
+    }
+
+    public TBNode<E> predcessorPostorder(TBNode<E> node) {
+        if(node.rightTag == '+')
+            return node.right;
+        if(node.leftTag == '-')
+            node = node.left;
+        return node.left;
+    }
+
+    public void postorderOppNonRecursive() {
+        if(head.leftTag == '-')
+            return; //empty tree
+
+        System.out.println("POSTORDER OPPOSITE (nonrecursive) :");
+        TBNode<E> p = head.left; //root
+
+        while(true) {
+            System.out.print(p.info.toString() + " ");
+            p = predcessorPostorder(p);
+            if(p == head.left)
+                break;
+        }
+        System.out.println();
+    }
+
+    public TBNode<E> insertRight(TBNode<E> parent, E info) {
+        TBNode<E> child = new TBNode<>(info);
+
+        child.leftTag = '-';
+        child.left = parent;
+        child.rightTag = parent.rightTag;
+        child.right = parent.right;
+
+        parent.right = child;
+        parent.rightTag = '+';
+
+        if(child.rightTag == '+') {
+            TBNode<E> temp = child.right;
+            while(temp.leftTag == '+')
+                temp = temp.left;
+            temp.left = child;
+        }
+
+        return child;
+    }
 }
